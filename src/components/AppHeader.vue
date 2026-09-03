3ded250e75183d07d46ebf8f2430bd9ff3aafefb
HELAL <elhamaimi2018@gmail.com>
2026-09-03 14:36:23 +0300

    feat: setup Vue Router and update navigation header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,32 +1,53 @@
-<script></script>
+```vue
+<script>
+export default {
+    name: "AppHeader",
+};
+</script>
 
 <template>
-    <nav class="navbar navbar-expand-lg bg-black">
-        <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg bg-black navbar-dark">
+        <div class="container-fluid px-3 px-lg-4">
             <!-- Logo -->
-            <a class="navbar-brand" href="#">
-                <img src="" alt="Bootstrap" width="30" height="24" />
-            </a>
+            <RouterLink to="/" class="navbar-brand">
+                <img src="" alt="Logo" width="30" height="24" />
+            </RouterLink>
 
-            <!-- Mobile Icons -->
+            <!-- Right Side Icons -->
             <div class="d-flex align-items-center gap-3 order-lg-3">
-                <!-- Search Icon - Mobile -->
-                <a href="#" class="text-white fs-5 d-lg-none">
+                <!-- Search - Mobile -->
+                <RouterLink
+                    to="/"
+                    class="text-white fs-5 d-lg-none text-decoration-none"
+                    aria-label="Search"
+                >
                     <i class="bi bi-search"></i>
-                </a>
+                </RouterLink>
 
                 <!-- Wishlist -->
-                <a href="#" class="text-white fs-5">
+                <RouterLink
+                    to="/wishlist"
+                    class="text-white fs-5 text-decoration-none"
+                    aria-label="Wishlist"
+                >
                     <i class="bi bi-heart"></i>
-                </a>
+                </RouterLink>
 
                 <!-- Profile -->
-                <a href="#" class="text-white fs-5">
+                <RouterLink
+                    to="/profile"
+                    class="text-white fs-5 text-decoration-none"
+                    aria-label="Profile"
+                >
                     <i class="bi bi-person"></i>
-                </a>
+                </RouterLink>
 
                 <!-- Cart -->
-                <a href="#" class="text-white fs-5 position-relative">
+                <RouterLink
+                    to="/cart"
+                    class="text-white fs-5 position-relative text-decoration-none"
+                    aria-label="Shopping Cart"
+                >
                     <i class="bi bi-cart3"></i>
 
                     <span
@@ -34,10 +55,10 @@
                     >
                         0
                     </span>
-                </a>
+                </RouterLink>
             </div>
 
-            <!-- Burger -->
+            <!-- Mobile Burger -->
             <button
                 class="navbar-toggler ms-2"
                 type="button"
@@ -50,29 +71,34 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
 
+            <!-- Navigation -->
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Links -->
+                <!-- Navigation Links -->
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active text-white" href="Shop.vue"> Shop </a>
+                        <RouterLink to="/" class="nav-link"> Shop </RouterLink>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="Categories.vue">
+                        <RouterLink to="/categories" class="nav-link">
                             Categories
-                        </a>
+                        </RouterLink>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="Deals.vue"> Deals </a>
+                        <RouterLink to="/deals" class="nav-link">
+                            Deals
+                        </RouterLink>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="Support.vue"> Support </a>
+                        <RouterLink to="/support" class="nav-link">
+                            Support
+                        </RouterLink>
                     </li>
                 </ul>
 
-                <!-- Search Box - Desktop -->
+                <!-- Desktop Search -->
                 <form
                     class="d-flex search-form mx-lg-auto my-3 my-lg-0"
                     role="search"
@@ -85,7 +111,11 @@
                             aria-label="Search"
                         />
 
-                        <button class="btn btn-outline-secondary" type="submit">
+                        <button
+                            class="btn btn-outline-secondary"
+                            type="submit"
+                            aria-label="Search"
+                        >
                             <i class="bi bi-search text-white"></i>
                         </button>
                     </div>
@@ -95,15 +125,43 @@
     </nav>
 </template>
 
-<style>
+<style scoped>
 .search-form {
     width: 100%;
     max-width: 450px;
 }
 
+.navbar .nav-link {
+    color: white !important;
+    transition: 0.2s ease;
+}
+
+.navbar .nav-link:hover {
+    opacity: 0.75;
+}
+
+/* Active RouterLink */
+.navbar .nav-link.router-link-exact-active {
+    color: white !important;
+    font-weight: 600;
+}
+
+/* Search */
+.search-form .form-control {
+    border-right: 0;
+}
+
+.search-form .btn {
+    border-left: 0;
+}
+
 @media (max-width: 991.98px) {
     .search-form {
         display: none !important;
+    }
+
+    .navbar-nav {
+        margin-top: 15px;
     }
 }
 </style>
